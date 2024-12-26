5f871b1945924d21acb2b1fe3c953674715eb603
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-12-26 18:51:26 -0500

    [O] Rewrite map area condition

--- src/main/java/icu/samnyan/aqua/sega/chusan/ChusanServletController.kt
+++ src/main/java/icu/samnyan/aqua/sega/chusan/ChusanServletController.kt
@@ -271,4 +271,40 @@ fun ChusanServletController.init() {
     "GetGameEvent" static { db.gameEvent.findByEnable(true).let { mapOf("type" to 1, "length" to it.size, "gameEventList" to it) } }
     "GetGameCharge" static { db.gameCharge.findAll().let { mapOf("length" to it.size, "gameChargeList" to it) } }
     "GetGameGacha" static { db.gameGacha.findAll().let { mapOf("length" to it.size, "gameGachaList" to it, "registIdList" to empty) } }
+    "GetGameMapAreaCondition" static {
+        mapOf(
+            "gameMapAreaConditionList" to listOf(
+                mapOf("mapAreaId" to 2206201, "mapAreaConditionList" to listOf(
+                    mapOf("type" to 3, "conditionId" to 6832, "logicalOpe" to 1)
+                )),
+                mapOf("mapAreaId" to 2206203, "mapAreaConditionList" to listOf(
+                    mapOf("type" to 3, "conditionId" to 6833, "logicalOpe" to 1)
+                )),
+                mapOf("mapAreaId" to 2206204, "mapAreaConditionList" to listOf(
+                    mapOf("type" to 3, "conditionId" to 6834, "logicalOpe" to 1),
+                    mapOf("type" to 3, "conditionId" to 6835, "logicalOpe" to 1)
+                )),
+                mapOf("mapAreaId" to 2206205, "mapAreaConditionList" to listOf(
+                    mapOf("type" to 3, "conditionId" to 6837, "logicalOpe" to 1)
+                )),
+                mapOf("mapAreaId" to 2206206, "mapAreaConditionList" to listOf(
+                    mapOf("type" to 3, "conditionId" to 6838, "logicalOpe" to 1)
+                )),
+                mapOf("mapAreaId" to 2206207, "mapAreaConditionList" to listOf(
+                    mapOf("type" to 2, "conditionId" to 2206201, "logicalOpe" to 1),
+                    mapOf("type" to 2, "conditionId" to 2206202, "logicalOpe" to 1),
+                    mapOf("type" to 2, "conditionId" to 2206203, "logicalOpe" to 1),
+                    mapOf("type" to 2, "conditionId" to 2206204, "logicalOpe" to 1),
+                    mapOf("type" to 2, "conditionId" to 2206205, "logicalOpe" to 1),
+                    mapOf("type" to 2, "conditionId" to 2206206, "logicalOpe" to 1)
+                )),
+                mapOf("mapAreaId" to 3229301, "mapAreaConditionList" to listOf(
+                    mapOf("type" to 1, "conditionId" to 3020701, "logicalOpe" to 2)
+                )),
+                mapOf("mapAreaId" to 3229302, "mapAreaConditionList" to listOf(
+                    mapOf("type" to 1, "conditionId" to 3020701, "logicalOpe" to 1)
+                ))
+            )
+        )
+    }
 }
